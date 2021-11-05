1a215cde796f9dea10e62cd8f7e0e0a985926d7d
Alexander <47079354+Nitolapio@users.noreply.github.com>
2021-11-05 11:48:27 +0100

    Funcionalidades de la pantalla creación de perfil

--- StravaClient/src/com/moma/fans/gui/ProfileCreationView.java
+++ StravaClient/src/com/moma/fans/gui/ProfileCreationView.java
@@ -1,11 +1,16 @@
 package com.moma.fans.gui;
 
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.binding.NumberBinding;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.util.StringConverter;
 
 public class ProfileCreationView extends VBox {
 
@@ -13,7 +18,7 @@ public class ProfileCreationView extends VBox {
 
         GridPane gp = new GridPane();
         gp.setAlignment(Pos.CENTER);
-        gp.setVgap(10.5);
+        gp.setVgap(15);
 
         // Campos obligatorios
 
@@ -29,6 +34,7 @@ public class ProfileCreationView extends VBox {
         // Campos opcionales
 
         Label lblWeight = new Label("Peso:");
+        Label lblKg = new Label("kg");
         SpinnerValueFactory<Double> valueFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0,200, 45, 0.1);
         Spinner<Double> spinWeight = new Spinner<>();
         spinWeight.setValueFactory(valueFactory);
@@ -36,20 +42,47 @@ public class ProfileCreationView extends VBox {
 
         Label lblMinHeartRate = new Label("Frecuencia cardiaca máxima:");
         Slider sldMinHeartRate = new Slider(50, 220, 80);
+        Label lblHeartRateMin = new Label(Double.toString(sldMinHeartRate.getValue()));
 
         Label lblMaxHeartRate = new Label("Frecuencia cardiaca mínima:");
-        Slider sldMaxHeartRate = new Slider(50, 220, 100);
+        Slider sldMaxHeartRate = new Slider(50, 220, 80);
+        Label lblHeartRateMax = new Label(Double.toString(sldMaxHeartRate.getValue()));
+
+        Button btnBack = new Button("Atrás");
+        Button btnOK = new Button("OK");
 
         gp.add(lblName, 0, 0); gp.add(tfName, 1, 0);
         gp.add(lblSurname, 0, 1); gp.add(tfSurname, 1, 1);
         gp.add(lblBirthDate, 0, 2); gp.add(dpBirth, 1, 2);
-        gp.add(lblWeight, 0, 3); gp.add(spinWeight, 1, 3);
-        gp.add(lblMinHeartRate, 0, 4); gp.add(sldMinHeartRate, 1, 4);
-        gp.add(lblMaxHeartRate, 0, 5); gp.add(sldMaxHeartRate, 1, 5);
+        gp.add(lblWeight, 0, 3); gp.add(spinWeight, 1, 3); gp.add(lblKg, 2,3);
+        gp.add(lblMinHeartRate, 0, 4); gp.add(sldMinHeartRate, 1, 4); gp.add(lblHeartRateMin, 2, 4);
+        gp.add(lblMaxHeartRate, 0, 5); gp.add(sldMaxHeartRate, 1, 5); gp.add(lblHeartRateMax, 2, 5);
+        gp.add(btnBack, 0, 7); gp.add(btnOK, 2, 7);
 
         final ColumnConstraints col = new ColumnConstraints();
         col.setPercentWidth( 50 );
         // gp.getColumnConstraints().addAll(col, col);
+
+        sldMinHeartRate.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                lblHeartRateMin.setText("" + (int) sldMinHeartRate.getValue());
+            }
+        });
+
+        sldMaxHeartRate.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                lblHeartRateMax.setText("" + (int) sldMaxHeartRate.getValue());
+            }
+        });
+
+        /* sampleRateSlider.valueProperty().addListener((ov, old_val, new_val) -> {
+            int value = (int) Math.round(new_val.doubleValue());
+            sampleRateSlider.setValue(value);
+            System.out.println(value);
+            sampleRateValueLabel.setText(getDisplayString(value));
+        }); */
 
         this.setAlignment(Pos.CENTER);
         this.getChildren().add(gp);
